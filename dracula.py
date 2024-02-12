d299c5006f38eb5dbf783b4ea8b68bdab28135bf
Mona Lisa <MonaLisa@example.com>
2024-02-12 22:37:42 +0000

    Playing with python pip packages

---
+++ dracula.py
@@ -0,0 +1,11 @@
+counter= 0
+
+with open("dracula.txt","r") as foo:
+    with open("vampytimes.txt","w") as fang:
+        for line in foo:
+            if "vampire" in line.lower():
+                print(line)
+                counter += 1
+                fang.write(line)
+
+print(counter)
